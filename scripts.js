dd9aca6fd4b80c41419b391d9e93dd6cf54d8967
Bryan Tran <bryanvantran@ucla.edu>
2016-10-22 15:43:39 -0700

    adding inputs

--- scripts.js
+++ scripts.js
@@ -2,34 +2,42 @@ var source   = $("#entry-template").html();
 var template = Handlebars.compile(source);
 
 var baseUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson";
-var startTime = "2014-01-02";
-var endTime = "2014-01-03";
 
-var query = baseUrl + '&starttime='+ startTime + '&endtime=' + endTime;
-console.log(query);
-$.ajax({
-            url: query,
-            type: "GET",
-            success: function(resultData) {
-                var features = resultData.features;
-                // console.log(features);
-                var html    = template(features);
-                $('#body').html(html);
-                // features.forEach(function(feature) {
-                //
-                //         // gives us the location of the earthquake
-                //         // console.log(feature.properties.place);
-                //         // gives us magnitude
-                //         // console.log(feature.properties.mag);
-                //         // this gives us each point, we can use this to plot points on google map
-                //         // console.log(feature.geometry);
-                //
-                //         // more information
-                //         // console.log(feature.properties.url);
-                // });
-            },
-            error : function(jqXHR, textStatus, errorThrown) {
-            },
+function query() {
+        var startTime = document.getElementById('start').value;
+        var endTime = document.getElementById('end').value;
+        var long = document.getElementById('long').value;
+        var lat = document.getElementById('lat').value;
+        var maxradius = "25";
 
-            timeout: 120000,
-        });
+        var query = baseUrl + '&starttime='+ startTime + '&endtime=' + endTime + '&maxradius=' + maxradius + '&longitude=' + long + '&latitude=' + lat;
+        console.log(query);
+        $.ajax({
+                    url: query,
+                    type: "GET",
+                    success: function(resultData) {
+                        var features = resultData.features;
+                        // console.log(features);
+                        var html    = template(features);
+                        $('#body').html(html);
+                        // features.forEach(function(feature) {
+                        //
+                        //         // gives us the location of the earthquake
+                        //         // console.log(feature.properties.place);
+                        //         // gives us magnitude
+                        //         // console.log(feature.properties.mag);
+                        //         // this gives us each point, we can use this to plot points on google map
+                        //         // console.log(feature.geometry);
+                        //
+                        //         // more information
+                        //         // console.log(feature.properties.url);
+                        // });
+                    },
+                    error : function(jqXHR, textStatus, errorThrown) {
+                    },
+
+                    timeout: 120000,
+                });
+}
+
+$("#submit").click(query);
